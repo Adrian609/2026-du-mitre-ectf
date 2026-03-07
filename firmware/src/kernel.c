9fc9bc095239b1b91b4f0b11797aa52e3c76a64a
Adrian <rodriguez.adrian609@gmail.com>
2026-03-06 19:34:40 -0700

    formated files

--- firmware/src/kernel.c
+++ firmware/src/kernel.c
@@ -15,74 +15,68 @@
 #include "ti_msp_dl_config.h"
 #include "security.h"
 
-
-
 /** @brief Prints the current processor configuration
  *
  * TODO: Remove
-*/
-void print_current_mode(void) {
-	uint32_t ipsr;
-	__asm volatile ("mrs %0, ipsr" : "=r"(ipsr));
+ */
+void print_current_mode(void)
+{
+    uint32_t ipsr;
+    __asm volatile("mrs %0, ipsr" : "=r"(ipsr));
 
-	uint32_t control;
-	__asm volatile ("mrs %0, control" : "=r"(control));
+    uint32_t control;
+    __asm volatile("mrs %0, control" : "=r"(control));
 
-	uint32_t stack;
-	__asm volatile ("mov %0, sp" : "=r"(stack));
+    uint32_t stack;
+    __asm volatile("mov %0, sp" : "=r"(stack));
 
-	uint32_t setting = control & 0x1;
+    uint32_t setting = control & 0x1;
 
-	char str[64];
+    char str[64];
 
-	sprintf(str, "Mode: %s | %s | Current SP: 0x%08X\n",
-               (ipsr != 0) ? "Handler" : "Thread",
-               (setting == 0) ? "Privileged" : "Unprivileged",
-               (uint32_t)stack);
+    sprintf(str, "Mode: %s | %s | Current SP: 0x%08X\n",
+            (ipsr != 0) ? "Handler" : "Thread",
+            (setting == 0) ? "Privileged" : "Unprivileged",
+            (uint32_t)stack);
 
-	print_debug(str);
-	
+    print_debug(str);
 }
 
-
-
 /** @brief Initializes hardware and switches to user mode
-*/
-KERNEL_CODE __attribute__((noinline, noreturn)) void start_kernel(void) {
+ */
+KERNEL_CODE __attribute__((noinline, noreturn)) void start_kernel(void)
+{
     // Initialize the device
     init();
 
-	// Set up and enable MPU regions
-	setup_mpu_layout();
+    // Set up and enable MPU regions
+    setup_mpu_layout();
 
     // Boot delay to reach around 1 second
-    uint32_t cycles_since_reset = 0xFFFFFF - SysTick->VAL;    
-    delay_cycles(DELAY_1_SEC - cycles_since_reset - 3*DELAY_1_MSEC); // 3ms slack
-    
-	// Switch to user mode and run user loop
-	__asm volatile ("svc %0\n" : : "i" (SYS_START_USER_LOOP));
+    uint32_t cycles_since_reset = 0xFFFFFF - SysTick->VAL;
+    delay_cycles(DELAY_1_SEC - cycles_since_reset - 3 * DELAY_1_MSEC); // 3ms slack
 
-	__builtin_unreachable();
+    // Switch to user mode and run user loop
+    __asm volatile("svc %0\n" : : "i"(SYS_START_USER_LOOP));
 
+    __builtin_unreachable();
 }
 
-
-
 /** @brief Initializes peripherals for system boot.
-*/
-KERNEL_CODE void init() {
-    // Initialize all of the hardware components
-	#if ON_BOARD
+ */
+KERNEL_CODE void init()
+{
+// Initialize all of the hardware components
+#if ON_BOARD
     SYSCFG_DL_init();
-	#endif
+#endif
 
     // Initialize file system
     init_fs();
-    
+
     // Initialize security related objects
     init_security();
 }
-
 
 /** @brief Memory Protection Regions
  *
@@ -91,7 +85,7 @@ KERNEL_CODE void init() {
  * Region 0: 0x0 - 0xFFFF (Kernel code and global data) Priv RX
  *           Bootloader: 0x0 - 0x5FFF (24KB)
  *           Kernel code: 0x6000 - 0xDFFF (32KB)
- *           Overriden by region 1: 0xE000 - 0xFFFF 
+ *           Overriden by region 1: 0xE000 - 0xFFFF
  *
  * Region 1: 0xE000 - 0xFFFF (8KB Secrets and other RO consts) Priv RO
  *
@@ -128,8 +122,9 @@ KERNEL_CODE void init() {
  * 7:6      Reserved (make 0)
  * 5:1      SIZE - region size; calculated as 2^(SIZE+1).
  * 0        ENABLE - region enable; must be 1 for the region to exist.
-*/
-KERNEL_CODE void setup_mpu_layout(void) {
+ */
+KERNEL_CODE void setup_mpu_layout(void)
+{
 
     MPU->CTRL = 0; // disable MPU during configuration
 
@@ -139,71 +134,68 @@ KERNEL_CODE void setup_mpu_layout(void) {
        Size: 64KB (8KB sub-regions)
        SRD 0x80 (10000000b) disables the 8th slice (0xE000-0xFFFF)
     */
-    MPU->RNR  = 0; 
+    MPU->RNR = 0;
     MPU->RBAR = 0x00000000;
     MPU->RASR = (0b00000101 << 24) | (0x80 << 8) | (0x0F << 1) | 1;
 
-
     /* Region 1: Secrets (0xE000 - 0xFFFF)
-	   Permission: Priv RO
+       Permission: Priv RO
        Size: 8KB; higher index overrides Region 0
     */
-    MPU->RNR  = 1; 
+    MPU->RNR = 1;
     MPU->RBAR = 0x0000E000;
     MPU->RASR = (0b00010101 << 24) | (0x00 << 8) | (0x0C << 1) | 1;
 
     /* Region 2: Files/FAT/Kernel-Stage (0x10000 - 0x3FFFF)
-	   Permission: Priv RW
+       Permission: Priv RW
        Size: 256KB (32KB sub-regions)
        SRD 0x03 (00000011b) disables first two slices (0x0-0xFFFF)
     */
-    MPU->RNR  = 2; 
+    MPU->RNR = 2;
     MPU->RBAR = 0x00000000;
     MPU->RASR = (0b00010001 << 24) | (0x03 << 8) | (0x11 << 1) | 1;
 
-	// --------USER-----------
+    // --------USER-----------
     /* Region 3: User App (0x24000 - 0x37FFF)
-	   Permission: User RX
+       Permission: User RX
        Size: 128KB (16KB sub-regions)
-       SRD 0xC1 (11000001b) disables first slice 0x20000-0x23FFF and 
+       SRD 0xC1 (11000001b) disables first slice 0x20000-0x23FFF and
          7-8th slices 0x38000-0x3FFFF; higher index overrides Region 2
     */
-    MPU->RNR  = 3; 
+    MPU->RNR = 3;
     MPU->RBAR = 0x00020000;
     MPU->RASR = (0b00000110 << 24) | (0xC1 << 8) | (0x10 << 1) | 1;
 
-	// --------SRAM-----------
+    // --------SRAM-----------
     /* Region 4: User RAM (0x20200000 - 0x20205FFF)
-	   Permission: User RW
+       Permission: User RW
        Size: 32KB
-	   SRD 0xC0 (11000000b) disables 0x20206000-0x20207FFF
+       SRD 0xC0 (11000000b) disables 0x20206000-0x20207FFF
     */
-    MPU->RNR  = 4; 
-	MPU->RBAR = 0x20200000;
-	MPU->RASR = (0b00010011 << 24) | (0xC0 << 8) | (0x0E << 1) | 1;
-    
+    MPU->RNR = 4;
+    MPU->RBAR = 0x20200000;
+    MPU->RASR = (0b00010011 << 24) | (0xC0 << 8) | (0x0E << 1) | 1;
+
     /* Region 5: Kernel RAM (0x20206000 - 0x20207FFF)
-	   Permission: Priv RW
+       Permission: Priv RW
        Size: 8KB
     */
-	MPU->RNR  = 5; 
-	MPU->RBAR = 0x20206000;
-	MPU->RASR = (0b00010001 << 24) | (0x00 << 8) | (0x0C << 1) | 1;
+    MPU->RNR = 5;
+    MPU->RBAR = 0x20206000;
+    MPU->RASR = (0b00010001 << 24) | (0x00 << 8) | (0x0C << 1) | 1;
 
-	// --------USER Accessible Peripheral-----------
+    // --------USER Accessible Peripheral-----------
     /* Region 6: UARTs 0&1 (0x40108000 - 0x4010BFFF)
-	   Permission: User RW
+       Permission: User RW
        Size: 16KB
     */
-    MPU->RNR  = 6; 
+    MPU->RNR = 6;
     MPU->RBAR = 0x40108000;
     MPU->RASR = (0b00010011 << 24) | (0x00 << 8) | (0x0D << 1) | 1;
 
-	// Enable MPU with unspecified regions as privileged only
-	MPU->CTRL = MPU_CTRL_ENABLE_Msk | MPU_CTRL_PRIVDEFENA_Msk;
+    // Enable MPU with unspecified regions as privileged only
+    MPU->CTRL = MPU_CTRL_ENABLE_Msk | MPU_CTRL_PRIVDEFENA_Msk;
 
-    __DSB(); __ISB();
-
+    __DSB();
+    __ISB();
 }
-
-
--- firmware/src/ti_msp_dl_config.c
+++ firmware/src/ti_msp_dl_config.c
@@ -55,7 +55,8 @@ SYSCONFIG_WEAK void SYSCFG_DL_init(void)
     SYSCFG_DL_UART_1_init();
 }
 
-SYSCONFIG_WEAK void SYSCFG_DL_TRNG_init(void) {
+SYSCONFIG_WEAK void SYSCFG_DL_TRNG_init(void)
+{
 
     DL_TRNG_reset(TRNG);
     DL_TRNG_enablePower(TRNG);
@@ -63,13 +64,12 @@ SYSCONFIG_WEAK void SYSCFG_DL_TRNG_init(void) {
     DL_TRNG_setClockDivider(TRNG, DL_TRNG_CLOCK_DIVIDE_2);
 
     DL_TRNG_sendCommand(TRNG, DL_TRNG_CMD_NORM_FUNC);
-    while (!DL_TRNG_isCommandDone(TRNG));
+    while (!DL_TRNG_isCommandDone(TRNG))
+        ;
     DL_TRNG_clearInterruptStatus(TRNG, DL_TRNG_INTERRUPT_CMD_DONE_EVENT);
 
     DL_TRNG_setDecimationRate(TRNG, DL_TRNG_DECIMATION_RATE_4);
-    
 }
-
 
 SYSCONFIG_WEAK void SYSCFG_DL_initPower(void)
 {
@@ -100,46 +100,40 @@ SYSCONFIG_WEAK void SYSCFG_DL_GPIO_init(void)
     DL_GPIO_initDigitalOutput(LEDS_STATUS_LED_IOMUX);
 
     DL_GPIO_initDigitalInputFeatures(BUTTONS_S2_IOMUX,
-		 DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_PULL_UP,
-		 DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
+                                     DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_PULL_UP,
+                                     DL_GPIO_HYSTERESIS_DISABLE, DL_GPIO_WAKEUP_DISABLE);
 
     DL_GPIO_setPins(GPIOB, LEDS_STATUS_LED_PIN);
     DL_GPIO_enableOutput(GPIOB, LEDS_STATUS_LED_PIN);
-
 }
-
 
 SYSCONFIG_WEAK void SYSCFG_DL_SYSCTL_init(void)
 {
 
-	//Low Power Mode is configured to be SLEEP0
+    // Low Power Mode is configured to be SLEEP0
     DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0);
 
     DL_SYSCTL_setSYSOSCFreq(DL_SYSCTL_SYSOSC_FREQ_BASE);
     DL_SYSCTL_setMCLKDivider(DL_SYSCTL_MCLK_DIVIDER_DISABLE);
-
 }
 
-
 static const DL_UART_Main_ClockConfig gUART_0ClockConfig = {
-    .clockSel    = DL_UART_MAIN_CLOCK_BUSCLK,
-    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1
-};
+    .clockSel = DL_UART_MAIN_CLOCK_BUSCLK,
+    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1};
 
 static const DL_UART_Main_Config gUART_0Config = {
-    .mode        = DL_UART_MAIN_MODE_NORMAL,
-    .direction   = DL_UART_MAIN_DIRECTION_TX_RX,
+    .mode = DL_UART_MAIN_MODE_NORMAL,
+    .direction = DL_UART_MAIN_DIRECTION_TX_RX,
     .flowControl = DL_UART_MAIN_FLOW_CONTROL_NONE,
-    .parity      = DL_UART_MAIN_PARITY_NONE,
-    .wordLength  = DL_UART_MAIN_WORD_LENGTH_8_BITS,
-    .stopBits    = DL_UART_MAIN_STOP_BITS_ONE
-};
+    .parity = DL_UART_MAIN_PARITY_NONE,
+    .wordLength = DL_UART_MAIN_WORD_LENGTH_8_BITS,
+    .stopBits = DL_UART_MAIN_STOP_BITS_ONE};
 
 SYSCONFIG_WEAK void SYSCFG_DL_UART_0_init(void)
 {
-    DL_UART_Main_setClockConfig(UART_0_INST, (DL_UART_Main_ClockConfig *) &gUART_0ClockConfig);
+    DL_UART_Main_setClockConfig(UART_0_INST, (DL_UART_Main_ClockConfig *)&gUART_0ClockConfig);
 
-    DL_UART_Main_init(UART_0_INST, (DL_UART_Main_Config *) &gUART_0Config);
+    DL_UART_Main_init(UART_0_INST, (DL_UART_Main_Config *)&gUART_0Config);
     /*
      * Configure baud rate by setting oversampling and baud rate divisors.
      *  Target baud rate: 115200
@@ -148,29 +142,25 @@ SYSCONFIG_WEAK void SYSCFG_DL_UART_0_init(void)
     DL_UART_Main_setOversampling(UART_0_INST, DL_UART_OVERSAMPLING_RATE_16X);
     DL_UART_Main_setBaudRateDivisor(UART_0_INST, UART_0_IBRD_32_MHZ_115200_BAUD, UART_0_FBRD_32_MHZ_115200_BAUD);
 
-
-
     DL_UART_Main_enable(UART_0_INST);
 }
 static const DL_UART_Main_ClockConfig gUART_1ClockConfig = {
-    .clockSel    = DL_UART_MAIN_CLOCK_BUSCLK,
-    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1
-};
+    .clockSel = DL_UART_MAIN_CLOCK_BUSCLK,
+    .divideRatio = DL_UART_MAIN_CLOCK_DIVIDE_RATIO_1};
 
 static const DL_UART_Main_Config gUART_1Config = {
-    .mode        = DL_UART_MAIN_MODE_NORMAL,
-    .direction   = DL_UART_MAIN_DIRECTION_TX_RX,
+    .mode = DL_UART_MAIN_MODE_NORMAL,
+    .direction = DL_UART_MAIN_DIRECTION_TX_RX,
     .flowControl = DL_UART_MAIN_FLOW_CONTROL_NONE,
-    .parity      = DL_UART_MAIN_PARITY_NONE,
-    .wordLength  = DL_UART_MAIN_WORD_LENGTH_8_BITS,
-    .stopBits    = DL_UART_MAIN_STOP_BITS_ONE
-};
+    .parity = DL_UART_MAIN_PARITY_NONE,
+    .wordLength = DL_UART_MAIN_WORD_LENGTH_8_BITS,
+    .stopBits = DL_UART_MAIN_STOP_BITS_ONE};
 
 SYSCONFIG_WEAK void SYSCFG_DL_UART_1_init(void)
 {
-    DL_UART_Main_setClockConfig(UART_1_INST, (DL_UART_Main_ClockConfig *) &gUART_1ClockConfig);
+    DL_UART_Main_setClockConfig(UART_1_INST, (DL_UART_Main_ClockConfig *)&gUART_1ClockConfig);
 
-    DL_UART_Main_init(UART_1_INST, (DL_UART_Main_Config *) &gUART_1Config);
+    DL_UART_Main_init(UART_1_INST, (DL_UART_Main_Config *)&gUART_1Config);
     /*
      * Configure baud rate by setting oversampling and baud rate divisors.
      *  Target baud rate: 115200
@@ -179,8 +169,5 @@ SYSCONFIG_WEAK void SYSCFG_DL_UART_1_init(void)
     DL_UART_Main_setOversampling(UART_1_INST, DL_UART_OVERSAMPLING_RATE_16X);
     DL_UART_Main_setBaudRateDivisor(UART_1_INST, UART_1_IBRD_32_MHZ_115200_BAUD, UART_1_FBRD_32_MHZ_115200_BAUD);
 
-
-
     DL_UART_Main_enable(UART_1_INST);
 }
-
